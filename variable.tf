ed86d384f2f42ec2d0efad60500ed05022d0ea87
autogithib <55689101+autogithib@users.noreply.github.com>
2019-09-23 18:42:40 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,17 @@
+provider "azurerm" {
+
+subscription_id = "f23700aa-eaa5-4898-95aa-d4dc3622116d"
+
+client_id = "${var.azure_client_id}"
+
+client_secret = "${var.azure_client_secret}"
+
+tenant_id = "608147da-af1b-435d-bb8d-b4139988da46"
+
+
+}
+
+
 variable "azure_client_id" {
 
 description = "Client ID for the Service Principal."
